78f3068e9c31401da6498816505c21192fe2bf33
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2025-02-19 20:04:40 +0330

    refactor battery notifier script

--- blocks/batteryNotifier.sh
+++ blocks/batteryNotifier.sh
@@ -3,5 +3,10 @@
 lower_bound=10
 upper_bound=70
 
-[[ $(cat /sys/class/power_supply/BAT0/status) == 'Charging' ]] && [[ $(cat /sys/class/power_supply/BAT0/capacity) -gt upper_bound ]] && notify-send 'unplug the laptop' -t 10000 ||
-  [[ $(cat /sys/class/power_supply/BAT0/status) == 'Discharging' ]] && [[ $(cat /sys/class/power_supply/BAT0/capacity) -lt lower_bound ]] && notify-send 'plug the laptop' -t 10000
+status=$(cat /sys/class/power_supply/BAT0/status)
+
+if [[ "$status" == 'Charging' ]] && [[ $(cat /sys/class/power_supply/BAT0/capacity) -gt $upper_bound ]]; then
+  notify-send 'unplug the laptop' -t 10000
+elif
+  [[ "$status" == 'Discharging' ]] && [[ $(cat /sys/class/power_supply/BAT0/capacity) -lt $lower_bound ]]
+then notify-send 'plug the laptop' -t 10000; fi
