4d5dbecd4cf2e5b9624c0850053c94126936a9c1
Mohammad amin Khajeh koolaki <mmdamin_khajehkoolaki256@protonmail.com>
2026-06-09 08:01:19 +0330

    Use `pamixer` instead of `pactl` to manipulate audio

--- blocks/volume.sh
+++ blocks/volume.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
 
 case $BLOCK_BUTTON in
-1) pactl set-sink-mute @DEFAULT_SINK@ toggle ;;
+1) pamixer --toggle-mute ;;
 3) st -e "$EDITOR" "$0" ;;
-4) pactl set-sink-volume @DEFAULT_SINK@ +2% ;;
-5) pactl set-sink-volume @DEFAULT_SINK@ -2% ;;
+4) pamixer --increase 2 ;;
+5) pamixer --decrease 2 ;;
 esac
 
 vol="$(pamixer --get-volume)"
-is_muted="$(pactl get-sink-mute @DEFAULT_SINK@ | grep -oE '[^ ]+$')"
-if [ "$is_muted" = 'yes' ]; then
+is_muted="$(pamixer --get-mute)"
+if [ "$is_muted" = 'true' ]; then
   printf "%s  " 
 else
   printf "%s  " 
